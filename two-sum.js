31fedcac92ab62a359bdbef04eaab37f6687957e
Mina Kaviani <mina.kavianig@gmail.com>
2025-09-25 21:16:08 +0330

    add another solution for two-sum.js

--- two-sum.js
+++ two-sum.js
@@ -1,4 +1,7 @@
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//this soloution is two-pass hash table
+// time complexity=O(n)
+// space complexity=O(n)
 var twoSum = function(nums, target) {
   const map=new Map();
   for(let i=0;i<nums.length;i++){
@@ -11,4 +14,20 @@ var twoSum = function(nums, target) {
     }
   }
   return[]; 
+};
+
+  //this soloution is one-pass hash table
+  // time complexity=O(n)
+// space complexity=O(n)
+  var twoSum = function(nums, target) {
+  const map=new Map();
+  for(let i=0;i<nums.length;i++) {
+    const complement=target-nums[i];
+    if(map.has(complement)){
+        return([map.get(complement),i])
+    }
+    map.set(nums[i],i)
+    
+  }
+  return [];
 };
